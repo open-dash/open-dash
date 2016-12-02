a66038942e2c4078298e9f843e88842cca9e8299
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-12-02 12:06:08 -0500

    add logging to Slack message method

--- server/methods/utils.js
+++ server/methods/utils.js
@@ -15,9 +15,17 @@ export default function () {
     'util/slackMessage'(message) {
       check(message, String);
 
+      const logger = Logger.child({
+        meteor_method: 'util/slackMessage',
+        meteor_method_args: [message],
+        userId: this.userId
+      });
+
       const slackWebhookUrl = Settings.get('slackWebhookUrl');
 
       if (process.env.SLACK_ENABLED && slackWebhookUrl) {
+        logger.info({ message }, 'Sending Slack message');
+
         return HTTP.call('POST', slackWebhookUrl, {
           headers: {
             'Content-Type': 'application/json'
@@ -28,6 +36,10 @@ export default function () {
           }
         });
       }
+
+      logger.warn({ message }, 'Slack is either not enabled or not configured. Message not sent');
+
+      return true;
     }
   });
 
