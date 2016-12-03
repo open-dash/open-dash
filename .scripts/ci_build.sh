85efdf64618238c262fcf83f9dc504fe7c3daaa7
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-12-03 12:50:24 -0500

    add CircleCI config and setup deployment

---
+++ .scripts/ci_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# load up cached image if there is one
+if [[ -e ~/docker/image.tar ]]; then
+  docker load -i ~/docker/image.tar
+fi
+
+# build new base and app images
+docker build -t jshimko/homedash:latest .
+
+# if successful, save in cache
+mkdir -p ~/docker
+docker save jshimko/homedash:latest > ~/docker/image.tar
+
+# run the container and wait for it to boot
+docker-compose up -d
+sleep 15
+
+# use curl to ensure the app returns 200's
+curl --retry 10 --retry-delay 10 -v http://localhost
