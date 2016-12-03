c569b5f110afd78d744e9dc7a151f8baaccbfb06
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-12-03 12:00:16 -0500

    package cleanup

--- client/configs/context.js
+++ client/configs/context.js
@@ -2,7 +2,6 @@ import * as Collections from '/lib/collections';
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { SubsManager } from 'meteor/meteorhacks:subs-manager';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tracker } from 'meteor/tracker';
 import { Notify, Alert } from '/client/modules/core/configs/notifications';
@@ -17,7 +16,6 @@ export default function () {
     Tracker,
     Notify,
     Alert,
-    SubsManager,
     LocalState: new ReactiveDict()
   };
 }
