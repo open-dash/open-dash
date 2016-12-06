2b322612c4481d2dd4d9570fc210c5e0ed825d1b
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-12-06 01:11:22 -0500

    break out lifx publication

--- server/publications/index.js
+++ server/publications/index.js
@@ -1,11 +1,13 @@
 import dashboard from './dashboard';
 import devices from './devices';
+import lifx from './lifx';
 import settings from './settings';
 import users from './users';
 
 export default function () {
   dashboard();
   devices();
+  lifx();
   settings();
   users();
 }
