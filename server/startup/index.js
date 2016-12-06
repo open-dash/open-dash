bb3c30eb9c16b7af2a40e0829a099b97e765fd1c
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-12-06 00:24:04 -0500

    add Kadira support

--- server/startup/index.js
+++ server/startup/index.js
@@ -4,4 +4,15 @@ import seed from './seed';
 export default function () {
   security();
   seed();
+
+  // connect to Kadira in production (if configured)
+  // https://kadira.io
+  if (process.env.NODE_ENV === 'production') {
+    const kadiraAppId = Settings.get('kadiraAppId');
+    const kadiraAppSecret = Settings.get('kadiraAppSecret');
+
+    if (kadiraAppId && kadiraAppSecret) {
+      Kadira.connect(kadiraAppId, kadiraAppSecret);
+    }
+  }
 }
