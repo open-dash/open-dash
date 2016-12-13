7cf4dcec3cb776e1600614cb54213b9ac971ce25
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-12-13 16:15:38 -0500

    clean up setting page state handling

--- client/modules/settings/components/settings_page.js
+++ client/modules/settings/components/settings_page.js
@@ -13,34 +13,23 @@ class SettingsPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      settings: props.settings
-    };
+    this.state =  props.settings;
 
     this.handleStateChange = this.handleStateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleStateChange(e) {
-    const { settings } = this.state;
-
-    const fieldState = {};
-    fieldState[e.target.name] = e.target.value;
-
-    const newState = Object.assign(settings, fieldState);
-
-    this.setState(newState);
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
     const { update } = this.props;
-    const { settings } = this.state;
-    update(settings);
+    update(this.state);
   }
 
   render() {
-    const { settings } = this.state;
 
     return (
       <Grid className='settings-page'>
@@ -49,7 +38,7 @@ class SettingsPage extends Component {
           <Col sm={12} md={6} mdOffset={3}>
             <Panel className='settings-page-form'>
               <h3 className='form-heading text-center'>Settings</h3>
-              <form onSubmit={this.handleSubmit.bind(this)}>
+              <form onSubmit={this.handleSubmit}>
 
                 <Row className='settings-group-heading'><h3>General</h3></Row>
 
@@ -57,20 +46,20 @@ class SettingsPage extends Component {
                   label='Site Title'
                   type='text'
                   name='siteTitle'
-                  value={settings.siteTitle}
+                  value={this.state.siteTitle}
                   onChange={this.handleStateChange}/>
                 <FieldGroup
                   label='Admin Email'
                   type='text'
                   name='adminEmail'
-                  value={settings.adminEmail}
+                  value={this.state.adminEmail}
                   onChange={this.handleStateChange}
                   info='(used for automated emails)'/>
                 <FieldGroup
                   label='Mail URL'
                   type='text'
                   name='mailUrl'
-                  value={settings.mailUrl}
+                  value={this.state.mailUrl}
                   onChange={this.handleStateChange}
                   info='(SMTP URL for sending app emails)'/>
 
@@ -80,7 +69,7 @@ class SettingsPage extends Component {
                   label='API Key'
                   type='text'
                   name='lifxApiKey'
-                  value={settings.lifxApiKey}
+                  value={this.state.lifxApiKey}
                   onChange={this.handleStateChange}/>
 
                 <Row className='settings-group-heading'><h3>Kadira</h3></Row>
@@ -89,13 +78,13 @@ class SettingsPage extends Component {
                   label='App ID'
                   type='text'
                   name='kadiraAppId'
-                  value={settings.kadiraAppId}
+                  value={this.state.kadiraAppId}
                   onChange={this.handleStateChange}/>
                 <FieldGroup
                   label='Secret'
                   type='text'
                   name='kadiraAppSecret'
-                  value={settings.kadiraAppSecret}
+                  value={this.state.kadiraAppSecret}
                   onChange={this.handleStateChange}/>
 
                 <Row className='settings-group-heading'><h3>Segment.io</h3></Row>
@@ -104,7 +93,7 @@ class SettingsPage extends Component {
                   label='API Key'
                   type='text'
                   name='segmentKey'
-                  value={settings.segmentKey}
+                  value={this.state.segmentKey}
                   onChange={this.handleStateChange}/>
 
                 <Row className='settings-group-heading'><h3>Slack</h3></Row>
@@ -113,7 +102,7 @@ class SettingsPage extends Component {
                   label='Webhook URL'
                   type='text'
                   name='slackWebhookUrl'
-                  value={settings.slackWebhookUrl}
+                  value={this.state.slackWebhookUrl}
                   onChange={this.handleStateChange}/>
 
                 <Row>
