953acc6b92bdbbc2db43c501609a369599a59448
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-12-21 11:58:35 -0500

    display ST devices in a table for now

--- client/modules/smartthings/components/devices.js
+++ client/modules/smartthings/components/devices.js
@@ -1,12 +1,27 @@
 import React, { PropTypes } from 'react';
-// import { Panel, Button } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 const SmartThingsDevices = ({ devices }) => (
-  <div>
-    <pre>
-      {JSON.stringify(devices, null, 2)}
-    </pre>
-  </div>
+  <Table bordered responsive>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Manufacturer</th>
+        <th>Model</th>
+      </tr>
+    </thead>
+    <tbody>
+      {devices.map((device, i) => (
+        <tr key={i}>
+          <td>{device.name}</td>
+          <td>{device.type}</td>
+          <td>{device.manufacturer}</td>
+          <td>{device.model}</td>
+        </tr>
+      ))}
+    </tbody>
+  </Table>
 );
 
 SmartThingsDevices.propTypes = {
