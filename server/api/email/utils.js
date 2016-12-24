9f65c8427675695cae01a5c06ca92faeb3773c68
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-12-24 11:12:40 -0500

    create renderTemplate method for email tempate SSR

--- server/api/email/utils.js
+++ server/api/email/utils.js
@@ -1,12 +1,27 @@
+import { Random } from 'meteor/random';
 import { Settings, Users } from '/lib/collections';
 import { Logger } from '/server/api';
 import { send } from './actions';
 
+
+/**
+ * Render an HTML template
+ * @param {String} template - path to html file (relative to /private)
+ * @param {Object} options - the email subject line
+ * @return {String} returns rendered html string
+ */
+export function renderTemplate(template, options = {}) {
+  const templateId = Random.id();
+  SSR.compileTemplate(templateId, Assets.getText(template));
+  return SSR.render(templateId, options);
+}
+
+
 /**
  * Send an email to ALL admins
- * @param  {String}  role - the role of users that should be notified
- * @param  {String}  subject - the email subject line
- * @param  {String}  html - the email body - value can be text or HTML
+ * @param {String}  role - the role of users that should be notified
+ * @param {String}  subject - the email subject line
+ * @param {String}  html - the email body - value can be text or HTML
  * @return {Boolean} returns true on success
  */
 export function notifyUsersInRole(role = 'superuser', subject, html) {
