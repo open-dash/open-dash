878b0c8bd29bc061f96d5110f2c7840e2e92cd4b
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-01-11 21:23:57 -0500

    clean up helmet component

--- client/modules/core/components/head.js
+++ client/modules/core/components/head.js
@@ -3,21 +3,27 @@ import Helmet from 'react-helmet';
 
 const Head = ({ title }) => (
   <Helmet
-    meta={[{
-      charset: 'utf-8'
-    }, {
-      name: 'viewport',
-      content: 'width=device-width, initial-scale=1.0'
-    }]}
     defaultTitle={title}
     titleTemplate={`${title} | %s`}
-    script={[{
-      type: 'text/javascript',
-      src: 'https://use.typekit.net/yaa3wrc.js'
-    }, {
-      type: 'text/javascript',
-      innerHTML: 'try{Typekit.load({ async: true });}catch(e){}'
-    }]}/>
+    meta={[
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0'
+      }
+    ]}
+    script={[
+      {
+        type: 'text/javascript',
+        src: 'https://use.typekit.net/yaa3wrc.js'
+      },
+      {
+        type: 'text/javascript',
+        innerHTML: 'try{Typekit.load({ async: true });}catch(e){}'
+      }
+    ]}/>
 );
 
 Head.propTypes = {
